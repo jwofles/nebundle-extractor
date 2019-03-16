0b037d9b4d4778af0bf9b6017f886a4dfdd1a5f6
jwofles <31262599+jwofles@users.noreply.github.com>
2019-03-16 16:32:36 +0000

    Added source files

---
+++ WindowsApp3/Form1.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.updater = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(34, 38)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(254, 35)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Extract"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'updater
+        '
+        Me.updater.AutoSize = True
+        Me.updater.Location = New System.Drawing.Point(31, 18)
+        Me.updater.Name = "updater"
+        Me.updater.Size = New System.Drawing.Size(257, 13)
+        Me.updater.TabIndex = 1
+        Me.updater.Text = "Drag and drop the nebundle you would like to extract"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(19, 19)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(183, 17)
+        Me.CheckBox1.TabIndex = 2
+        Me.CheckBox1.Text = "Purge files smaller than 100 bytes"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(19, 42)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 207)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(53, 13)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "by jwofles"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.CheckBox1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(15, 108)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(293, 74)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(31, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 13)
+        Me.Label2.TabIndex = 8
+        Me.Label2.Text = "No file loaded"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(320, 229)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.updater)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Nebundle Extractor"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents updater As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+End Class
---
+++ WindowsApp3/Form1.vb
@@ -0,0 +1,139 @@
+﻿Imports System.IO
+Public Class Form1
+    Dim filePresent As Boolean = False
+    Dim filePath As String
+    Dim fileName As String
+    Dim bytePurge As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        openAndWrite()
+    End Sub
+
+    Sub openAndWrite()
+        'Dim fs As New FileStream(filePath, FileMode.Open)
+        'Dim bw As New BinaryWriter(fs)
+        updater.Text = ("This may take a while...")
+        Dim imageNum As Integer = 0
+        Dim fileIsOpen As Boolean = False
+        Dim bytes() As Byte '= {137, 80, 78, 71, 13, 10, 26, 10}
+        bytes = File.ReadAllBytes(filePath)
+        Button1.Enabled = False
+
+        'For Each number In bytes
+        'Dim pngHeader() As Byte = {137, 80, 78, 71, 13, 10, 26, 10}
+        If (Not System.IO.Directory.Exists(My.Computer.FileSystem.SpecialDirectories.Desktop & "\" & fileName & "_extracted")) Then
+            System.IO.Directory.CreateDirectory(My.Computer.FileSystem.SpecialDirectories.Desktop & "\" & fileName & "_extracted")
+        End If
+
+        'File.WriteAllBytes("C:\Users\henry\Desktop\" & fileName & "_extracted\" & imageNum & ".png", bytes)
+
+        Dim ihdrBytes() As Byte = {0, 0, 0, 13, 73, 72, 68, 82}
+        Dim iendBytes() As Byte = {73, 69, 78, 68, 174, 66, 96, 130}
+        Dim findLen As Integer = ihdrBytes.Length
+        Dim retLen As Integer = 10
+        Dim imageBytes As List(Of Byte) = New List(Of Byte)
+        For i As Integer = 0 To bytes.Length - 1
+            If bytes(i) = ihdrBytes(0) Then
+                If bytes.Skip(i).Take(findLen).SequenceEqual(ihdrBytes) Then
+                    'Dim output() As Byte = bytes.Skip(x).Take(retLen).ToArray
+                    imageNum += 1
+                    Label2.Text = ("Currently extracting image" & imageNum)
+                    Application.DoEvents()
+                    fileIsOpen = True
+                    imageBytes.Clear()
+                    ''WB.Write({137, 80, 78, 71, 13, 10, 26, 10}) ' PNG HEADER
+                    'WB.Write({0, 0, 0, 13, 73, 72, 68, 82})
+                End If
+            End If
+            If fileIsOpen = True Then
+                ''WB.Write(bytes(i))
+                imageBytes.Add(bytes(i))
+            End If
+
+            If bytes(i) = iendBytes(0) Then
+                If bytes.Skip(i).Take(findLen).SequenceEqual(iendBytes) Then
+                    'Dim output() As Byte = bytes.Skip(x).Take(retLen).ToArray
+                    Dim path As String = My.Computer.FileSystem.SpecialDirectories.Desktop & "\" & fileName & "_extracted\image" & imageNum & ".png"
+
+                    Dim FS As New FileStream(path, FileMode.Create)
+                    Dim WB As New BinaryWriter(FS)
+                    WB.Write({137, 80, 78, 71, 13, 10, 26, 10}) ' PNG HEADER
+                    WB.Write(imageBytes.ToArray)
+                    WB.Write({73, 69, 78, 68, 174, 66, 96, 130}) 'iend
+                    fileIsOpen = False
+                    FS.Close()
+                    Dim fi As New IO.FileInfo(path)
+                    If fi.Length <= bytePurge Then
+                        My.Computer.FileSystem.DeleteFile(path)
+                        Application.DoEvents()
+                        'Label2.Text = ("Image" & imageNum & " only " & fi.Length & " bytes, it has been purged")
+                        imageNum -= 1
+                    End If
+                End If
+            End If
+        Next
+
+        'MsgBox(pngHeader)
+        'If number = 0 And number + 1 = 0 And number + 2 = 0 And number + 3 = 13 Then
+        '    MsgBox("found")
+        '    Dim pngByteList() As Byte
+        '    imageNum += 1
+        '    File.WriteAllBytes("\" & fileName & "\" & imageNum & ".png", pngByteList)
+        'End If
+        'Next
+        Button1.Enabled = True
+        MsgBox("Finished")
+        updater.Text = ("Drag And drop the nebundle you would Like to extract")
+    End Sub
+
+    'Sub writeAndSave()
+    '    Dim fs As New FileStream(filePath, FileMode.Create)
+    '    Dim bw As New BinaryWriter(fs)
+    '    For imagenum = 0 To 123
+    '        File.Create("C:\Desktop\" & fileName & "\" & imagenum & ".png").Dispose()
+    '    Next
+    'End Sub
+
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Me.AllowDrop = True
+        Button1.Enabled = False
+        TextBox1.Text = ("100")
+        CheckBox1.Checked = True
+        'TextBox1.Enabled = True
+    End Sub
+
+    Private Sub Form1_DragDrop(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles Me.DragDrop
+        Dim files() As String = e.Data.GetData(DataFormats.FileDrop)
+        Button1.Enabled = True
+        For Each path In files
+            filePath = path
+            updater.Text = (path)
+            fileName = System.IO.Path.GetFileName(path)
+        Next
+        label2.Text = ("File loaded")
+    End Sub
+
+    Private Sub Form1_DragEnter(sender As System.Object, e As System.Windows.Forms.DragEventArgs) Handles Me.DragEnter
+        If e.Data.GetDataPresent(DataFormats.FileDrop) Then
+            e.Effect = DragDropEffects.Copy
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        CheckBox1.Text = ("Purge files smaller than ") & TextBox1.Text & (" bytes")
+        Try
+            bytePurge = Convert.ToInt32(TextBox1.Text)
+        Catch
+            bytePurge = 0
+            CheckBox1.Text = ("Purge files smaller than ?? bytes")
+        End Try
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+        If CheckBox1.Checked = True Then
+            TextBox1.Enabled = True
+        Else
+            TextBox1.Enabled = False
+        End If
+    End Sub
+
+End Class
